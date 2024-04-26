621b1579fed3e177b5c99808cc45ff11927cfbaf
shivang2056 <64776242+shivang2056@users.noreply.github.com>
2024-04-26 14:41:13 +0530

    Task: Modify routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :splits
-  resources :items
-  resources :expenses
   devise_for :users
+
+  resources :expenses do
+    resources :items do
+      resources :splits
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
