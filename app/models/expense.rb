71b6b84b306af2b3c033fe91a41281a4df1a8cd7
shivang2056 <64776242+shivang2056@users.noreply.github.com>
2024-05-15 20:19:35 +0530

    Task: Add expense model tests for scopes.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -6,23 +6,45 @@ class Expense < ApplicationRecord
 
   accepts_nested_attributes_for :items
 
-  scope :related_to_user, -> (user) { where(user: user).or(where(splits: { user: user })).joins(:splits).distinct.order(created_at: :desc) }
+  scope :related_to_user, -> (user) {
+    where(user: user)
+      .or(
+          where(splits: {
+            user: user
+          })
+      )
+      .joins(:splits)
+      .distinct
+      .order(created_at: :desc)
+  }
 
   scope :between_users, -> (user1, user2) {
-                                            joins(:splits)
-                                            .where(expenses: {user_id: user1.id}, splits: {user_id: user2.id})
-                                            .or(
-                                              joins(:splits)
-                                              .where(expenses: {user_id: user2.id}, splits: {user_id: user1.id})
-                                            )
+    joins(:splits)
+      .where(expenses: {
+        user_id: user1.id
+      }, splits: {
+        user_id: user2.id
+      })
+      .or(
+        joins(:splits)
+        .where(expenses: {
+          user_id: user2.id
+        }, splits: {
+          user_id: user1.id
+        })
+      )
   }
 
   scope :with_users_as_participants_only, -> (user1, user2) {
-                                            joins(:splits)
-                                            .where(splits: {user_id: [user1.id, user2.id]})
-                                            .where.not(expenses: {user_id: [user1.id, user2.id]})
-                                            .group('expenses.id')
-                                            .having('COUNT(DISTINCT splits.user_id) >= 2')
+    joins(:splits)
+      .where(splits: {
+        user_id: [user1.id, user2.id]
+      })
+      .where.not(expenses: {
+        user_id: [user1.id, user2.id]
+      })
+      .group('expenses.id')
+      .having('COUNT(DISTINCT splits.user_id) >= 2')
   }
 
 end
--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -1,7 +1,134 @@
-require "test_helper"
+require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user1 = users(:user1)
+    @user2 = users(:user2)
+    @user3 = users(:user3)
+    @user4 = users(:user4)
+  end
+
+  class RelatedToUserTest < ExpenseTest
+    def setup
+      super
+
+      @expense1 = Expense.create(user: @user1,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 10, user: @user2)
+                                ])])
+      @expense2 = Expense.create(user: @user3,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 20, user: @user2)
+                                ])])
+      @expense3 = Expense.create(user: @user2,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 10, user: @user1)
+                                ])])
+      @expense4 = Expense.create(user: @user3,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 20, user: @user1)
+                                ])])
+    end
+
+    test 'should return expenses created by user' do
+      assert_includes Expense.related_to_user(@user1), @expense1
+      assert_not_includes Expense.related_to_user(@user1), @expense2
+    end
+
+    test 'should return empty when no expenses related to user' do
+      assert_empty Expense.related_to_user(@user4)
+    end
+
+    test 'should return expenses where user is participant' do
+      assert_includes Expense.related_to_user(@user1), @expense3
+      assert_not_includes Expense.related_to_user(@user1), @expense2
+    end
+
+    test 'should return expenses where user is participant or user created the expense' do
+      assert_includes Expense.related_to_user(@user1), @expense1
+      assert_includes Expense.related_to_user(@user1), @expense3
+      assert_includes Expense.related_to_user(@user1), @expense4
+      assert_not_includes Expense.related_to_user(@user1), @expense2
+    end
+  end
+
+  class BetweenUsersTest < ExpenseTest
+    def setup
+      super
+
+      @expense1 = Expense.create(user: @user1,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 10, user: @user2)
+                                ])])
+      @expense2 = Expense.create(user: @user1,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 20, user: @user3)
+                                ])])
+      @expense3 = Expense.create(user: @user2,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 10, user: @user1)
+                                ])])
+      @expense4 = Expense.create(user: @user2,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 20, user: @user3)
+                                ])])
+      @expense5 = Expense.create(user: @user1,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 10, user: @user2),
+                                  Split.new(amount: 20, user: @user3)
+                                ])])
+      @expense6 = Expense.create(user: @user1,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 10, user: @user3),
+                                  Split.new(amount: 20, user: @user4)
+                                ])])
+    end
+
+    test 'should return expenses when user1 created the expense and user2 is participant' do
+      assert_includes Expense.between_users(@user1, @user2), @expense1
+      assert_not_includes Expense.between_users(@user1, @user2), @expense2
+    end
+
+    test 'should return expenses when user2 created the expense and user1 is participant' do
+      assert_includes Expense.between_users(@user1, @user2), @expense3
+      assert_not_includes Expense.between_users(@user1, @user2), @expense4
+    end
+
+    test 'should return expenses when user1 created the expense and user2 is participant along with others' do
+      assert_includes Expense.between_users(@user1, @user2), @expense5
+      assert_not_includes Expense.between_users(@user1, @user2), @expense6
+    end
+
+    test 'should return empty when no expenses exist involving both users' do
+      assert_empty Expense.between_users(@user3, @user4)
+    end
+  end
+
+  class WithUsersAsParticipantsOnlyTest < ExpenseTest
+    def setup
+      super
+
+      @expense1 = Expense.create(user: @user1,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 10, user: @user3),
+                                  Split.new(amount: 20, user: @user4)
+                                ])])
+      @expense2 = Expense.create(user: @user3,
+                                items: [Item.new(splits: [
+                                  Split.new(amount: 10, user: @user3),
+                                  Split.new(amount: 20, user: @user4)
+                                ])])
+    end
+
+    test 'should return expenses when both users are participant but none of the user created the expense' do
+      assert_includes Expense.with_users_as_participants_only(@user3, @user4), @expense1
+      assert_not_includes Expense.with_users_as_participants_only(@user3, @user4), @expense2
+    end
+
+    test 'should return empty if no expense exist with both users as participant only' do
+      assert_empty Expense.with_users_as_participants_only(@user1, @user4)
+      assert_empty Expense.with_users_as_participants_only(@user2, @user4)
+      assert_empty Expense.with_users_as_participants_only(@user2, @user3)
+    end
+  end
 end
